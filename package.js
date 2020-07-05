fe3e181ce854bedf11ebfea9228e3fa0caca6085
Laurent <laurent@laurentroger.com>
2020-07-05 18:02:07 +0200

    update npm versions

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Npm.depends({
-  'mjml': '4.1.2',
-  'handlebars': '4.0.5'
+  mjml: '4.6.3',
+  handlebars: '4.7.6',
 });
 
 Package.describe({
   name: 'simonhochrein:mjml',
-  version: '0.1.4',
+  version: '0.1.5',
   summary: 'MJML Email Template Engine For Meteor',
   git: 'https://github.com/simonhochrein/meteor-mjml',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.3.4.1');
   api.use('ecmascript');
-  api.use([ 'email' ], 'server');
-  api.addFiles([ 'mjml.js' ], 'server');
+  api.use(['email'], 'server');
+  api.addFiles(['mjml.js'], 'server');
   api.export('MJML', 'server');
 });
